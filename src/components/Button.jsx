c854986ad70ece589f62fb1e9c204059b22673b7
Basit <basitalikorai7@gmail.com>
2023-10-16 19:52:37 +0500

    style: home - bottom spacing fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = () => {
   return (
     <a
-      className="grid place-content-center bg-white text-space font-serif px-[2em] aspect-square relative z-10 max-w-[9.375rem] max-h-[9.375rem] md:max-w-[17.125rem] md:max-h-[17.125rem] rounded-full text-[1.25rem] md:text-h4 after:content-[''] after:top-0 after:right-0 after:bg-navcolor after:absolute after:h-full after:w-full after:-z-10 after:rounded-full after:transition-all after:opacity-0 hover:after:opacity-100 hover:after:scale-150"
+      className="grid place-content-center bg-white text-space font-serif px-[2em] aspect-square relative z-10 max-w-[9.375rem] max-h-[9.375rem] md:max-w-[17.125rem] md:max-h-[17.125rem] rounded-full text-[1.25rem] md:text-h4 after:content-[''] after:top-0 after:right-0 after:bg-navcolor after:absolute after:h-full after:w-full after:-z-10 after:rounded-full after:transition-all after:opacity-0 hover:after:opacity-100 tall:hover:after:scale-[1.4] hover:after:scale-150"
       href="#"
     >
       EXPLORE
