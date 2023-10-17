2666e80b8de489f1b5b614b6e31e40d5241ff316
Basit <basitalikorai7@gmail.com>
2023-10-17 12:26:03 +0500

    code refactored

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,5 +1,15 @@
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
 const Destination = () => {
-  return <div>Destination</div>
+  return (
+    <div>
+      <header>
+        <Navbar />
+        <Sidebar />
+      </header>
+    </div>
+  )
 }
 
 export default Destination
