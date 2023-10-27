2f0bc269c1434d65cc2d6e109054497fd227bae0
Basit <basitalikorai7@gmail.com>
2023-10-27 21:32:45 +0500

    fix: crew page state added

--- src/components/RoundIndicators.jsx
+++ src/components/RoundIndicators.jsx
@@ -1,23 +1,52 @@
-const RoundIndicators = ({ hideOn }) => {
+import { useGlobalContext } from '../context/context'
+import data from '../data/data.json'
+const team = data.crew
+
+const RoundIndicators = ({ hideOn, currentCrew, updateCrew }) => {
+  const isCrew1 = currentCrew === `Douglas Hurley`
+  const isCrew2 = currentCrew === `Mark Shuttleworth`
+  const isCrew3 = currentCrew === `Victor Glover`
+  const isCrew4 = currentCrew === `Anousheh Ansari`
+
   return (
     <div
       className={`round-indicators text-center flex gap-4 justify-center lg:justify-start items-center my-8 md:my-10 lg:mt-[7.5rem] ${hideOn}`}
     >
       <button
-        aria-selected="true"
-        class="pb-4 bg-white aspect-square rounded-full"
+        onClick={() => {
+          updateCrew(team[0])
+        }}
+        aria-selected={isCrew1}
+        className={`pb-4 ${
+          isCrew1 ? 'bg-white' : 'bg-grey hover:bg-translucent'
+        } aspect-square rounded-full`}
       ></button>
       <button
-        aria-selected="true"
-        class="pb-4 hover:bg-translucent bg-grey aspect-square rounded-full"
+        onClick={() => {
+          updateCrew(team[1])
+        }}
+        aria-selected={isCrew2}
+        className={`pb-4 ${
+          isCrew2 ? 'bg-white' : 'bg-grey hover:bg-translucent'
+        } aspect-square rounded-full`}
       ></button>
       <button
-        aria-selected="true"
-        class="pb-4 hover:bg-translucent bg-grey aspect-square rounded-full"
+        onClick={() => {
+          updateCrew(team[2])
+        }}
+        aria-selected={isCrew3}
+        className={`pb-4 ${
+          isCrew3 ? 'bg-white' : 'bg-grey hover:bg-translucent'
+        } aspect-square rounded-full`}
       ></button>
       <button
-        aria-selected="true"
-        class="pb-4 hover:bg-translucent bg-grey aspect-square rounded-full"
+        onClick={() => {
+          updateCrew(team[3])
+        }}
+        aria-selected={isCrew4}
+        className={`pb-4 ${
+          isCrew4 ? 'bg-white' : 'bg-grey hover:bg-translucent'
+        } aspect-square rounded-full`}
       ></button>
     </div>
   )
--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,11 +1,20 @@
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
-import AppProvider from '../context/context'
+import AppProvider, { useGlobalContext } from '../context/context'
 import backgroundImages from '../utils/backgroundImages'
-import crew1 from '../assets/images/crew/image-douglas-hurley.png'
 import RoundIndicators from '../components/RoundIndicators'
 
 const Crew = () => {
+  const {
+    crew: { name, role, bio },
+    setCrew,
+  } = useGlobalContext()
+
+  const isCrew1 = name === `Douglas Hurley`
+  const isCrew2 = name === `Mark Shuttleworth`
+  const isCrew3 = name === `Victor Glover`
+  const isCrew4 = name === `Anousheh Ansari`
+
   return (
     <AppProvider>
       <div className={`bg-crew ${backgroundImages}`}>
@@ -20,17 +29,25 @@ const Crew = () => {
           </p>
           <div className="grid lg:grid-cols-2 place-content-center text-left">
             <div className="md:order-last">
-              <div className="mx-auto mt-8 lg:mt-0 grid justify-items-center border-b border-navcolor md:border-transparent max-w-[20.4375rem] md:max-w-[27.75rem] lg:max-w-full">
-                <img
-                  className="max-w-[13.875rem] md:max-w-[28.52325rem] lg:max-w-[35.5045rem] "
-                  src={crew1}
-                  alt=""
-                  width="510"
-                  height="550"
-                />
-              </div>
+              <div
+                className={`lg:w-[28.34rem] min-h-[33.25rem] max-h-[70rem] mx-auto mt-8 lg:mt-0 grid justify-items-center border-b border-navcolor md:border-transparent max-w-[20.4375rem] md:max-w-[27.75rem] lg:max-w-full xl:absolute bottom-0 right-[11.6875rem]  bg-no-repeat bg-bottom bg-contain ${
+                  isCrew1
+                    ? 'crew1'
+                    : isCrew2
+                    ? 'crew2'
+                    : isCrew3
+                    ? 'crew3'
+                    : isCrew4
+                    ? 'crew4'
+                    : 'crew1'
+                }`}
+              ></div>
             </div>
-            <RoundIndicators hideOn={'lg:hidden'} />
+            <RoundIndicators
+              hideOn={'lg:hidden'}
+              updateCrew={setCrew}
+              currentCrew={name}
+            />
             <div className="md:order-first grid justify-center">
               <div>
                 <p className="hidden lg:block md:pl-[2.375rem] lg:pl-0 text-base md:text-xl lg:text-[1.75rem] uppercase font-sans_cond tracking-[0.16875rem] text-center lg:text-left mb-8 md:mb-[3.75rem] lg:mb-[9.63rem]">
@@ -39,18 +56,19 @@ const Crew = () => {
                 </p>
                 <p className="grid gap-2 lg:gap-[0.94rem] mb-4 md:mb-[1.75rem] lg:mb-[1.69rem] max-w-[20.4375rem] md:max-w-[27.75rem] lg:max-w-full pt-8 md:pt-0 mt-8 md:mt-0 mx-auto uppercase text-center lg:text-left font-serif tracking-normal leading-none">
                   <span className="text-sh2 md:text-[1.5rem] opacity-50">
-                    Commander
+                    {role}
                   </span>
-                  <span className="text-sh1 md:text-[2.5rem]">
-                    Douglas Hurley
-                  </span>
+                  <span className="text-sh1 md:text-[2.5rem]">{name}</span>
                 </p>
                 <p className="max-lg:mx-auto text-center lg:text-left text-[0.9375rem] md:text-base max-w-[20.4375rem] md:max-w-[27.75rem] leading-normal md:leading-7">
-                  Douglas Gerald Hurley is an American engineer, former Marine
-                  Corps pilot and former NASA astronaut. He launched into space
-                  for the third time as commander of Crew Dragon Demo-2.
+                  {bio}
                 </p>
-                <RoundIndicators hideOn={'hidden lg:flex'} />
+                {isCrew1 && <p></p>}
+                <RoundIndicators
+                  hideOn={'hidden lg:flex'}
+                  updateCrew={setCrew}
+                  currentCrew={name}
+                />
               </div>
             </div>
           </div>
