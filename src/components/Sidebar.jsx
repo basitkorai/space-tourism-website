e0163c93c30e6e4cb0f6aa175a118d9363ae27a1
Basit <basitalikorai7@gmail.com>
2023-11-17 17:38:56 +0500

    feat: focus trap added while sidebar is expanded

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from 'react'
 import iconClose from '../assets/images/shared/icon-close.svg'
 import { SideNavItem } from './NavListItem'
 import { motion } from 'framer-motion'
-import { ClickAwayListener } from '@mui/base'
+import { ClickAwayListener, FocusTrap } from '@mui/base'
 
 const Sidebar = () => {
   const closeSidebarBtn = useRef(null)
@@ -30,25 +30,27 @@ const Sidebar = () => {
         className={`md:hidden bg-navcolor backdrop-blur-spaceblur fixed top-0 bottom-0 right-0 w-[70%] h-screen z-50`}
         role="navigation"
       >
-        <ul id="sidenav">
-          <div className="grid justify-end pt-[1.62rem] pb-[3.56rem] pr-[1.15rem] text-base">
-            <button
-              ref={closeSidebarBtn}
-              tabIndex={isSidebarOpen ? 0 : -1}
-              className="btn-close xside py-2 px-[0.55rem] rounded-full"
-              onClick={() => {
-                setIsSidebarOpen(false)
-              }}
-              aria-label="close sidebar"
-            >
-              <img src={iconClose} alt="" width="20" height="20" />
-            </button>
-          </div>
-          <SideNavItem page="HOME" ItemNum="00" route="/" />
-          <SideNavItem page="DESTINATION" ItemNum="01" route="/destination" />
-          <SideNavItem page="CREW" ItemNum="02" route="/crew" />
-          <SideNavItem page="TECHNOLOGY" ItemNum="03" route="/technology" />
-        </ul>
+        <FocusTrap open={isSidebarOpen}>
+          <ul id="sidenav">
+            <div className="grid justify-end pt-[1.62rem] pb-[3.56rem] pr-[1.15rem] text-base">
+              <button
+                ref={closeSidebarBtn}
+                tabIndex={isSidebarOpen ? 0 : -1}
+                className="btn-close xside py-2 px-[0.55rem] rounded-full"
+                onClick={() => {
+                  setIsSidebarOpen(false)
+                }}
+                aria-label="close sidebar"
+              >
+                <img src={iconClose} alt="" width="20" height="20" />
+              </button>
+            </div>
+            <SideNavItem page="HOME" ItemNum="00" route="/" />
+            <SideNavItem page="DESTINATION" ItemNum="01" route="/destination" />
+            <SideNavItem page="CREW" ItemNum="02" route="/crew" />
+            <SideNavItem page="TECHNOLOGY" ItemNum="03" route="/technology" />
+          </ul>
+        </FocusTrap>
       </motion.nav>
     </ClickAwayListener>
   )
